d7ef0790b7796a5e341fa807805b1d82c2747cde
MaiveCL <maive_@hotmail.com>
2025-10-08 11:23:05 -0400

    déplacement de mainwondows dans views

--- Views/MainWindow.xaml.cs
+++ Views/MainWindow.xaml.cs
@@ -9,7 +9,7 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
-namespace TP2
+namespace TP2.Views
 {
     /// <summary>
     /// Interaction logic for MainWindow.xaml
